6b4cbfc6fb962558ee6615d50185e44a2a983d91
Alexis Semple <alexis.semple@epfl.ch>
2015-11-05 13:23:06 +0100

    PlanState class

--- src/template/CentralizedTemplate.java
+++ src/template/CentralizedTemplate.java
@@ -4,11 +4,9 @@ package template;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
+
 import logist.LogistSettings;
 
-import logist.Measures;
-import logist.behavior.AuctionBehavior;
 import logist.behavior.CentralizedBehavior;
 import logist.agent.Agent;
 import logist.config.Parsers;
@@ -37,25 +35,24 @@ public class CentralizedTemplate implements CentralizedBehavior {
     private int[] nextDeliver;
     private int[] time; // [p1, p2, ..., pn, d1, d2, ..., dn]
     private int[][] load;
-    
+
     @Override
     public void setup(Topology topology, TaskDistribution distribution,
-            Agent agent) {
-        
+                      Agent agent) {
+
         // this code is used to get the timeouts
         LogistSettings ls = null;
         try {
             ls = Parsers.parseSettings("config/settings_default.xml");
-        }
-        catch (Exception exc) {
+        } catch (Exception exc) {
             System.out.println("There was a problem loading the configuration file.");
         }
-        
+
         // the setup method cannot last more than timeout_setup milliseconds
         timeout_setup = ls.get(LogistSettings.TimeoutKey.SETUP);
         // the plan method cannot execute more than timeout_plan milliseconds
         timeout_plan = ls.get(LogistSettings.TimeoutKey.PLAN);
-        
+
         this.topology = topology;
         this.distribution = distribution;
         this.agent = agent;
@@ -64,12 +61,12 @@ public class CentralizedTemplate implements CentralizedBehavior {
     @Override
     public List<Plan> plan(List<Vehicle> vehicles, TaskSet tasks) {
         long time_start = System.currentTimeMillis();
-        
+
 //		System.out.println("Agent " + agent.id() + " has tasks " + tasks);
-        nextPickup = new int[tasks.size()+vehicles.size()];
-        nextDeliver = new int[tasks.size()+vehicles.size()];
-        time = new int[tasks.size()*2];
-        load = new int[vehicles.size()][2*tasks.size()];
+        nextPickup = new int[tasks.size() + vehicles.size()];
+        nextDeliver = new int[tasks.size() + vehicles.size()];
+        time = new int[tasks.size() * 2];
+        load = new int[vehicles.size()][2 * tasks.size()];
 
         initSolution(vehicles, tasks);
         Plan planVehicle1 = naivePlan(vehicles.get(0), tasks);
@@ -79,11 +76,11 @@ public class CentralizedTemplate implements CentralizedBehavior {
         while (plans.size() < vehicles.size()) {
             plans.add(Plan.EMPTY);
         }
-        
+
         long time_end = System.currentTimeMillis();
         long duration = time_end - time_start;
-        System.out.println("The plan was generated in "+duration+" milliseconds.");
-        
+        System.out.println("The plan was generated in " + duration + " milliseconds.");
+
         return plans;
     }
 
@@ -115,7 +112,7 @@ public class CentralizedTemplate implements CentralizedBehavior {
     private void initSolution(List<Vehicle> vehicles, TaskSet tasks) {
         int maxCap = 0;
         int index = -1;
-        for (Vehicle v: vehicles) {
+        for (Vehicle v : vehicles) {
             if (v.capacity() > maxCap) {
                 maxCap = v.capacity();
                 index = v.id();
@@ -131,21 +128,60 @@ public class CentralizedTemplate implements CentralizedBehavior {
         time[prev + tasks.size()] = ++t;
         load[index][t] = 0; // task has been delivered
 
-        while(it.hasNext()) {
+        while (it.hasNext()) {
             int next = it.next().id;
             nextPickup[prev] = next;
-            load[index][t] = getTask(tasks, prev).weight;
+            load[index][t] = getTask(tasks, prev).weight; // 0 or weight in load in initialised state.
             time[next] = ++t;
             time[next + tasks.size()] = ++t;
+            load[index][t] = 0;
         }
     }
-    
-	private Task getTask(TaskSet tasks, int id) {
-		for(Task t: tasks) {
-			if(t.id == id) {
-				return t;
-			}
-		}
-		return null;
-	}
+
+    private Task getTask(TaskSet tasks, int id) {
+        for (Task t : tasks) {
+            if (t.id == id) {
+                return t;
+            }
+        }
+        return null;
+    }
+
+    class PlanState {
+
+        private int[] nextPickup;
+        private int[] timeP; // [p1, p2, ..., pn]
+        private int[] timeD; // [d1, d2, ..., dn]
+        private int[][] load;
+
+        public PlanState(List<Vehicle> vehicles, TaskSet tasks) {
+            nextPickup = new int[tasks.size() + vehicles.size()];
+            timeP = new int[tasks.size()];
+            timeD = new int[tasks.size()];
+            load = new int[vehicles.size()][2 * tasks.size()];
+        }
+
+        public PlanState(List<Vehicle> vehicles, TaskSet tasks, PlanState p) {
+            nextPickup = p.getNextPickup().clone();
+            timeP = p.getTimeP().clone();
+            timeD = p.getTimeD().clone();
+            load = getLoad().clone();
+        }
+
+        public int[] getNextPickup() {
+            return nextPickup;
+        }
+
+        public int[] getTimeP() {
+            return timeP;
+        }
+
+        public int[] getTimeD() {
+            return timeD;
+        }
+
+        public int[][] getLoad() {
+            return load;
+        }
+    }
 }
