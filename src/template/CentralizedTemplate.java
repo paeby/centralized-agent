f49ec95d30edb8f71c8cf7bc36fe8ec04e62234e
Prisca Aeby <prisca_aeby@msn.com>
2015-11-06 08:40:31 +0100

    local choice updated with iterator

--- src/template/CentralizedTemplate.java
+++ src/template/CentralizedTemplate.java
@@ -1,14 +1,6 @@
 package template;
 
 //the list of imports
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.*;
 import logist.LogistSettings; 
 import logist.behavior.CentralizedBehavior;
@@ -69,7 +61,7 @@ public class CentralizedTemplate implements CentralizedBehavior {
 
         Plan planVehicle1 = naivePlan(vehicles.get(0), tasks);
 
-        List<Plan> plans = new ArrayList<>();
+        List<Plan> plans = new ArrayList<Plan>();
         plans.add(planVehicle1);
         while (plans.size() < vehicles.size()) {
             plans.add(Plan.EMPTY);
@@ -95,7 +87,7 @@ public class CentralizedTemplate implements CentralizedBehavior {
             @Override
             public Plan buildPlan(PlanState state, Vehicle v) {
                 Integer next = state.getNextPickup()[tasks.size() + v.id()];
-                Task t = getTask(tasks, state.getNextPickup()[tasks.size() + v.id()]);
+                Task t = getTask(tasks, next);
                 City current = v.homeCity();
                 Plan p = new Plan(current);
                 ArrayIterator itP = new ArrayIterator(state.getTimeP(), state.getVTasks().get(v.id()));
@@ -117,86 +109,18 @@ public class CentralizedTemplate implements CentralizedBehavior {
                         for (City c: current.pathTo(getTask(tasks, pickupIndex).pickupCity)){
                             p.appendMove(c);
                         }
+                        current = getTask(tasks, pickupIndex).pickupCity;
                         p.appendPickup(getTask(tasks, pickupIndex));
                     } else {
                         for (City c: current.pathTo(getTask(tasks, deliverIndex).deliveryCity)){
                             p.appendMove(c);
                         }
+                        current = getTask(tasks, deliverIndex).deliveryCity;
                         p.appendDelivery(getTask(tasks, deliverIndex));
                     }
                     //TODO nextPickup still necessary with hashSet of tasks for each vehicle?
                 }
-                
-                
-                
-                //TODO!!!!!!!DIRTY IMPLEMENTATION!!!!!! 
-                // IDEA: change the function local choice, would calculate the cost simply by 
-                // taking the total distance of each plan calculated in this function!
-                // actually not good idea to build a plan before the end... mhhhh..... problem.... brain fried...
-                
-                City newCity;
-    			ArrayList<Integer> vTasks = new ArrayList<Integer>();
-    			Set<Integer> delivered = new HashSet<Integer>();
-    			if(state.getNextPickup()[v.id()] != null) {
-    				
-    				// moving to the first city
-    				int firstPickup = state.getNextPickup()[tasks.size()+v.id()];
-    				vTasks.add(firstPickup);
-    				newCity = getTask(tasks, firstPickup).pickupCity;
-    				p.appendMove(newCity); // moving to pickup first task
-    				
-    				// moving to the next pickup or delivery city
-    				int nextPickup = firstPickup;
-    				while(state.getNextPickup()[nextPickup] != null) {
-    					nextPickup = state.getNextPickup()[nextPickup];
-    					vTasks.add(nextPickup); // all the tasks of a vehicle
-    					int timeP = state.getTimeP()[nextPickup];
-    					
-    					// Check if there are tasks to deliver before the task to pickup
-    					int minTime = timeP;
-    					Map<Integer, Integer> toDeliver = new HashMap<Integer, Integer>();
-    					for(Integer vTask: vTasks) {
-    						int timeD = state.getTimeD()[vTask];
-    						if(timeD < minTime && (!delivered.contains(t))) {
-    							toDeliver.put(timeD, vTask);
-    						}
-    					}
-    					
-    					while(toDeliver.size() != 0) {
-    						int minD = Collections.min(toDeliver.keySet());
-    						int deliver = toDeliver.get(minD);
-    						toDeliver.remove(minD);
-    						delivered.add(deliver);
-    						City currentCity = newCity;
-    						newCity = getTask(tasks, deliver).deliveryCity;
-    						p.appendMove(newCity);
-    					}
-    					
-    					//cost to the pickup city
-    					City prevCity = newCity;
-    					newCity = getTask(tasks, nextPickup).pickupCity;
-    				}
-
-    				// we still need to deliver all the remaining tasks
-    				Map<Integer, Integer> toDeliver = new HashMap<Integer, Integer>();
-    				for(Integer vTask: vTasks) {
-    					if(!delivered.contains(t)) {
-    						toDeliver.put(state.getTimeD()[vTask], vTask);
-    					}
-    				}
-    				
-    				while(toDeliver.size()!=0) {
-    					int minD = Collections.min(toDeliver.keySet());
-						int deliver = toDeliver.get(minD);
-						toDeliver.remove(minD);
-						delivered.add(deliver);
-						City currentCity = newCity;
-						newCity = getTask(tasks, deliver).deliveryCity;
-						p.appendMove(newCity);
-    				}
-    			}
-
-                return null; //return Plan for vehicle v
+                return p;
             }
         };
 
@@ -269,80 +193,44 @@ public class CentralizedTemplate implements CentralizedBehavior {
     	for(PlanState neighbour: neighbours) {
     		double cost = 0;
     		for(Vehicle v: vehicles) {
-    			City newCity;
-    			ArrayList<Integer> vTasks = new ArrayList<Integer>();
-    			Set<Integer> delivered = new HashSet<Integer>();
-    			if(neighbour.getNextPickup()[v.id()] != null) {
-    				
-    				// moving to the first city
-    				int firstPickup = neighbour.getNextPickup()[tasks.size()+v.id()];
-    				vTasks.add(firstPickup);
-    				newCity = getTask(tasks, firstPickup).pickupCity;
-    				cost += v.homeCity().distanceTo(newCity)*v.costPerKm(); // moving to pickup first task
-    				
-    				// moving to the next pickup or delivery city
-    				int nextPickup = firstPickup;
-    				while(neighbour.getNextPickup()[nextPickup] != null) {
-    					nextPickup = neighbour.getNextPickup()[nextPickup];
-    					vTasks.add(nextPickup); // all the tasks of a vehicle
-    					int timeP = neighbour.getTimeP()[nextPickup];
-    					
-    					// Check if there are tasks to deliver before the task to pickup
-    					int minTime = timeP;
-    					Map<Integer, Integer> toDeliver = new HashMap<Integer, Integer>();
-    					for(Integer t: vTasks) {
-    						int timeD = neighbour.getTimeD()[t];
-    						if(timeD < minTime && (!delivered.contains(t))) {
-    							toDeliver.put(timeD, t);
-    						}
-    					}
-    					
-    					while(toDeliver.size() != 0) {
-    						int minD = Collections.min(toDeliver.keySet());
-    						int deliver = toDeliver.get(minD);
-    						toDeliver.remove(minD);
-    						delivered.add(deliver);
-    						City currentCity = newCity;
-    						newCity = getTask(tasks, deliver).deliveryCity;
-    						cost += currentCity.distanceTo(newCity)*v.costPerKm();
-    					}
-    					
-    					//cost to the pickup city
-    					City prevCity = newCity;
-    					newCity = getTask(tasks, nextPickup).pickupCity;
-    					cost += prevCity.distanceTo(newCity);
-    				}
-    				
-    				//TODO CLEAN THIS DIRTY CODE
-    				// we still need to deliver all the remaining tasks
-    				Map<Integer, Integer> toDeliver = new HashMap<Integer, Integer>();
-    				for(Integer t: vTasks) {
-    					if(!delivered.contains(t)) {
-    						toDeliver.put(neighbour.getTimeD()[t], t);
-    					}
-    				}
-    				
-    				while(toDeliver.size()!=0) {
-    					int minD = Collections.min(toDeliver.keySet());
-						int deliver = toDeliver.get(minD);
-						toDeliver.remove(minD);
-						delivered.add(deliver);
-						City currentCity = newCity;
-						newCity = getTask(tasks, deliver).deliveryCity;
-						cost += currentCity.distanceTo(newCity)*v.costPerKm();
-    				}
-    			}
+    			 Integer next = neighbour.getNextPickup()[tasks.size() + v.id()];
+                 Task t = getTask(tasks, next);
+                 City current = v.homeCity();
+                 ArrayIterator itP = new ArrayIterator(neighbour.getTimeP(), neighbour.getVTasks().get(v.id()));
+                 ArrayIterator itD = new ArrayIterator(neighbour.getTimeD(), neighbour.getVTasks().get(v.id()));
+                 while(itP.hasNext() || itD.hasNext()) {
+                     int pickupTime = Integer.MAX_VALUE;
+                     int pickupIndex = -1;
+                     int deliverTime = Integer.MAX_VALUE;
+                     int deliverIndex = -1;
+                     if(itP.hasNext()) {
+                         pickupTime = itP.next();
+                         pickupIndex = itP.indexOf(pickupTime);
+                     }
+                     if(itD.hasNext()) {
+                         deliverTime = itD.next();
+                         deliverIndex = itD.indexOf(deliverTime);
+                     }
+                     if(pickupTime < deliverTime) {
+                         cost += current.distanceTo(getTask(tasks, pickupIndex).pickupCity)*v.costPerKm();
+                         current = getTask(tasks, pickupIndex).pickupCity;
+               
+                     } else {
+                         cost += current.distanceTo(getTask(tasks, deliverIndex).deliveryCity);
+                         current = getTask(tasks, deliverIndex).deliveryCity;
+                     }
+                 }
     		}
     		if(cost < min) {
-        		bestPlan = neighbour;
-        		min = cost;
-        	}
+    			min = cost;
+    			bestPlan = neighbour;
+    		}
     	}
     	return bestPlan;
     }
     
     private List<PlanState> ChooseNeighbours(PlanState plan, TaskSet tasks, List<Vehicle> vehicles){
-    	List<PlanState> neighbours = new ArrayList<>();
+    	List<PlanState> neighbours = new ArrayList<PlanState>();
     	
     	return neighbours;
     }
@@ -362,7 +250,7 @@ public class CentralizedTemplate implements CentralizedBehavior {
         private Integer[] timeP; // [p0, p1, ..., pn]
         private Integer[] timeD; // [d0, d1, ..., dn]
         private Integer[][] load;
-        private Map<Integer, HashSet<Integer>> vTasks = new HashMap<>(); // Map from vehicle_id to Set of tasks in vehicle's track
+        private Map<Integer, HashSet<Integer>> vTasks = new HashMap<Integer, HashSet<Integer>>(); // Map from vehicle_id to Set of tasks in vehicle's track
 
         /**
          * Constructor for empty PlanState
@@ -431,7 +319,7 @@ public class CentralizedTemplate implements CentralizedBehavior {
     /**
      * Iterates over an int array from min to max
      */
-    class ArrayIterator implements Iterator {
+    class ArrayIterator implements Iterator<Object> {
 
         private List<Integer> l;
         private HashSet<Integer> tasks;
